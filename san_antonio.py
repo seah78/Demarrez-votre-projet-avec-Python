6e430592be4c45a2190bd6999b248859ef688298
seah78 <github@seah.pro>
2018-11-11 08:43:51 +0100

    Créez votre premier script

---
+++ san_antonio.py
@@ -0,0 +1,27 @@
+# -*-coding: utf8 -*-
+
+quotes = [
+	"Ecoutez-moi, monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
+	"On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+
+]
+
+characters = [
+	"Alvin et les Chipmunks",
+	"Babar",
+	"Betty Boop",
+	"Calimero",
+	"Casper",
+	"Le chat potté",
+	"Kirikou"
+]
+
+
+# Show random quote
+
+
+# If user_ansmwer is "B":
+	# leave the program
+	
+# Else:
+ # show another quote		
