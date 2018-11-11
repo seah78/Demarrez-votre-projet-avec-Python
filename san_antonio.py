6fe23da5b6b95f57b76547d5cac35b645a5237ed
seah78 <github@seah.pro>
2018-11-11 16:27:12 +0100

    Structurez votre programme en utilisant les fonctions

--- san_antonio.py
+++ san_antonio.py
@@ -16,7 +16,7 @@ characters = [
 	"Kirikou"
 ]
 
-
+user_answer = input("Tapez entrée pour connaître une autre citation ou B pour quitter le programme.")
 # Show random quote
 
 
@@ -27,14 +27,21 @@ if user_answer == "B":
 	# leave the program
 	pass
 
-
 # Else:
 elif user_answer == "C":
 	print("C pas la bonne réponse ! Et G pas d'humour, je c...")
 	
-
-
 else:
 	
 	# show another quote
- 		
+
+	def show_random_quote(my_list):
+		# get a random number	
+		item = my_list[0]
+		
+		# get a quote from an array
+		return item
+
+	print(show_random_quote(quotes))
+	
+	
