033a36a27d00fe1e8612c0320f1bc0554f85930b
seah78 <github@seah.pro>
2018-11-13 07:44:00 +0100

    [BONUS] Stockez des citations dans un fichier avec le module json

--- san_antonio.py
+++ san_antonio.py
@@ -1,36 +1,34 @@
 # -*-coding: utf8 -*-
 import random
+import json
 
-quotes = [
-	"Ecoutez-moi, monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
-	"On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+def read_values_from_json(nfile, nkey):
+	values = []
+	with open(nfile) as f:
+		data = json.load(f)
+		for entry in data:
+			values.append(entry[nkey])
+	return values
 
-]
-
-characters = [
-	"alvin et les Chipmunks",
-	"Babar",
-	"betty Boop",
-	"calimero",
-	"casper",
-	"ce chat potté",
-	"Kirikou"
-]
-
-def show_random_item(my_list):
-	rand_numb = random.randint(0, len(my_list)-1)	#radom number
-	item = my_list[rand_numb]								# get a quote from a list
-	return item													#return the item
-	
 def message(character,quote):
 	n_character = character.capitalize()
 	n_quote = quote.capitalize()
 	return "{} a dit : {}".format(n_character, n_quote)
+
+def get_random_item_in(my_list):
+	rand_numb = random.randint(0, len(my_list)-1)	#radom number
+	item = my_list[rand_numb]								# get a quote from a list
+	return item													#return the item
+
+def get_random_item (xfile, xkey):
+	all_values = read_values_from_json(xfile, xkey)
+	return get_random_item_in(all_values)
 	
+#Programm	
 user_answer = input("Tapez entrée pour connaître une citation ou B pour quitter le programme : ")
 
 while user_answer != "B":
-	print(message(show_random_item(characters), show_random_item(quotes)))
+	print(message(get_random_item('characters.json', 'character'), get_random_item('quotes.json', 'quote')))
 	user_answer = input("Tapez entrée pour connaître une autre citation ou B pour quitter le programme : ")
 	
 	
