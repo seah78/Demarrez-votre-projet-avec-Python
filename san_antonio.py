fe54b0d71cc847216956d6da2c7efc00f426672d
seah78 <github@seah.pro>
2018-11-14 07:21:58 +0100

    [BONUS] Collectez des citations automatiquement avec Scrapy

--- san_antonio.py
+++ san_antonio.py
@@ -2,33 +2,55 @@
 import random
 import json
 
-def read_values_from_json(nfile, nkey):
+# Give a json file and return a list
+def read_values_from_json(path, nkey):
 	values = []
-	with open(nfile) as f:
+	with open(path) as f:
 		data = json.load(f)
 		for entry in data:
 			values.append(entry[nkey])
 	return values
 
-def message(character,quote):
-	n_character = character.capitalize()
-	n_quote = quote.capitalize()
-	return "{} a dit : {}".format(n_character, n_quote)
+# Give a json and return a list
+def clean_strings(sentences):
+	cleaned = []
+	# Strore quotes on a list. Create an empty list and add each sentence one by one.
+	for sentence in sentences:
+		# Clean quotes from whitespaces and so on
+		clean_sentence = sentence.strip()
+		# don't use extend as it adds each letter one by one !
+		cleaned.append(clean_sentence)
+	return cleaned
 
+# Return a random item in a list
 def get_random_item_in(my_list):
-	rand_numb = random.randint(0, len(my_list)-1)	#radom number
-	item = my_list[rand_numb]								# get a quote from a list
-	return item													#return the item
+	rand_numb = random.randint(0, len(my_list)-1)
+	item = my_list[rand_numb]	
+	return item
 
-def get_random_item (xfile, xkey):
-	all_values = read_values_from_json(xfile, xkey)
+# Return a random value from a json file
+def get_random_item (source_path, xkey):
+	all_values = read_values_from_json(source_path, xkey)
+	clean_values = clean_strings(all_values)
 	return get_random_item_in(all_values)
-	
-#Programm	
-user_answer = input("Tapez entrée pour connaître une citation ou B pour quitter le programme : ")
 
-while user_answer != "B":
-	print(message(get_random_item('characters.json', 'character'), get_random_item('quotes.json', 'quote')))
-	user_answer = input("Tapez entrée pour connaître une autre citation ou B pour quitter le programme : ")
-	
+# Print a random sentence.
+def print_random_sentence():
+	rand_quote = get_random_item('quotes.json', 'quote')
+	rand_character = get_random_item('characters.json', 'character')
+	print(">>>> {} a dit : {}".format(rand_character, rand_quote))
+
+#Programm	
+def main_loop():
+	while True:
+		print_random_sentence()
+		message = ('Voulez-vous voir une autre citation ?'
+						'Pour sortir du programme, tapes [B].')
+		choice = input(message).upper()
+		if choice == 'B':
+			break
+			# this will stop the loop !
+
+if __name__ == '__main__':
+	main_loop()
 	
